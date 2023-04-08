2aec6098a71d41d59f856bb8f8d3b94dd6e4ca54
Arya <32606430+Finura99@users.noreply.github.com>
2023-04-08 19:44:11 +0100

    Finalised the logic, added rounded function for easier readability

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=96d9da1c92f2a9f62c42724f37f85357`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=96d9da1c92f2a9f62c42724f37f85357`
 
 
   const searchLocation = (event) => {
@@ -33,34 +33,45 @@ function App() {
                 </div>
                 
       <div className="container">
+
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>25°c</h1>
+          {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main} </p> : null}
           </div>
+
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">24°c</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold">20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+
+
+
+        {data.name !== undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className="bold">{data.main.feels_like}°C</p> : null}
+                <p>Feels Like</p>
+              </div>
+
+              <div className="humidity">
+                {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+
+              <div className="wind">
+                {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null} 
+                <p>Wind Speed</p>
+              </div>
+
+            </div>
 }
+        </div>
+          </div>
+  )
+};
+
 
 export default App;
